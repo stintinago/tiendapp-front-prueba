62ea222a49ddc4a1417901e8166ebb0c3cd6954a
Santiago Tintinago <tinti261999@gmail.com>
2022-07-11 22:01:03 -0500

    last

--- src/components/AddBrand/index.js
+++ src/components/AddBrand/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
@@ -7,77 +7,97 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function AddBrand() {
 
-    const navigate = useNavigate();
+  //validation
+  const [validated, setValidated] = useState(false);
 
-    const [name, setName] = useState("")
-    const [reference, setReference] = useState(0)
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-    const [validationError, setValidationError] = useState({})
+    setValidated(true);
+  };
 
-    const createBrand = async (e) => {
-        e.preventDefault();
-    
-        const formData = new FormData()
-    
-        formData.append('name', name)
-        formData.append('reference', reference)
-    
-        await axios.post(`http://localhost:8000/api/brands`, formData).then(({ data }) => {
-          Swal.fire({
-            icon: "success",
-            text: data.message
-          })
-          navigate("/")
-        }).catch(({ response }) => {
-          if (response.status === 422) {
-            setValidationError(response.data.errors)
-          } else {
-            Swal.fire({
-              text: response.data.message,
-              icon: "error"
-            })
-          }
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("")
+  const [reference, setReference] = useState(0)
+
+  const [validationError, setValidationError] = useState({})
+
+  const createBrand = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData()
+
+    formData.append('name', name)
+    formData.append('reference', reference)
+
+    await axios.post(`http://localhost:8000/api/brands`, formData).then(({ data }) => {
+      Swal.fire({
+        icon: "success",
+        text: data.message
+      })
+      navigate("/")
+    }).catch(({ response }) => {
+      if (response.status === 422) {
+        setValidationError(response.data.errors)
+      } else {
+        Swal.fire({
+          text: response.data.message,
+          icon: "error"
         })
-    
       }
+    })
 
-    return (
-        <>
-            <Container className="mt-4">
+  }
 
-                <Form>
-                    <Row>
+  return (
+    <>
+      <Container className="mt-4">
 
-                        <Form.Group as={Col} md="4">
-                            <Form.Label>Brand Name</Form.Label>
-                            <Form.Control
-                                value={name}
-                                onChange={(event)=>setName(event.target.value)}
-                                required
-                                type="text"
-                            />
-                        </Form.Group>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row>
 
-                        <Form.Group as={Col} md="4">
-                            <Form.Label>Brand reference</Form.Label>
-                            <Form.Control
-                                value={reference}
-                                onChange={(event)=>setReference(event.target.value)}
-                                required
-                                type="number"
-                            />
-                        </Form.Group>
-                    </Row>
+            <Form.Group as={Col} md="4">
+              <Form.Label>Brand Name</Form.Label>
 
-                    <Button onClick={createBrand} className="mt-5" variant="primary">Add brand</Button>
+                <Form.Control
+                  
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  required
+                  type="text"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please type a name.
+                </Form.Control.Feedback>
 
-                </Form>
+            </Form.Group>
 
-            </Container>
-        </>
-    );
+            <Form.Group as={Col} md="4">
+              <Form.Label>Brand reference</Form.Label>
+              <Form.Control
+                value={reference}
+                onChange={(event) => setReference(event.target.value)}
+                required
+                type="number"
+              />
+            </Form.Group>
+          </Row>
+
+          <Button type="submit" onClick={createBrand} className="mt-5" variant="primary">Add brand</Button>
+
+        </Form>
+
+      </Container>
+    </>
+  );
 }
 export default AddBrand;
