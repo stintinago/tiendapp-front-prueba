45a709f9f2b8cd4c45cd748090b788c4f70dc768
Santiago Tintinago <tinti261999@gmail.com>
2022-07-11 19:37:29 -0500

    added brand list

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,6 +6,7 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import EditModal from './EditModal';
 import axios from 'axios';
+import Swal from 'sweetalert2'
 
 function Brand() {
 
@@ -16,11 +17,45 @@ function Brand() {
     const [brands, setBrands] = useState([])
 
     const fetchBrands = async () => {
-        await axios.get(`http://localhost:8000/api/brands`).then(({data})=>{
+        await axios.get(`http://localhost:8000/api/brands`).then(({ data }) => {
             setBrands(data)
         })
     }
 
+    useEffect(() => {
+        fetchBrands()
+    }, [])
+
+    const deleteBrand = async (id) => {
+        const isConfirm = await Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          return result.isConfirmed
+        });
+    
+        if (!isConfirm) {
+          return;
+        }
+    
+        await axios.delete(`http://localhost:8000/api/products/${id}`).then(({ data }) => {
+          Swal.fire({
+            icon: "success",
+            text: data.message
+          })
+          fetchBrands()
+        }).catch(({ response: { data } }) => {
+          Swal.fire({
+            text: data.message,
+            icon: "error"
+          })
+        })
+      }
     return (
         <>
             <Container className="mt-4">
@@ -33,25 +68,20 @@ function Brand() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td><Button onClick={null}>Edit</Button><Button className="mx-2" variant="danger">Delete</Button></td>
-                        </tr>
-                        <tr>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td><Button>Edit</Button><Button className="mx-2" variant="danger">Delete</Button></td>
-                        </tr>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td><Button>Edit</Button><Button className="mx-2" variant="danger">Delete</Button></td>
-                        </tr>
+                        {
+                            brands.length > 0 ? brands.map((row, key) => (
+
+                                <tr key={key}>
+                                    <td>{row.name}</td>
+                                    <td>{row.reference}</td>
+                                    <td><Button >Edit</Button><Button onClick={()=>deleteBrand(row.id)} className="mx-2" variant="danger">Delete</Button></td>
+                                </tr>
+                            )) : <h1>No data</h1>
+                    }
                     </tbody>
                 </Table>
             </Container>
-            
+
         </>
     )
 }
